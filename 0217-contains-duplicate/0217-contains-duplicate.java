d6159a4a120a1cca49fafa2014258836ad0d9299
Patel Vandan Ashishkumar <vandanp1705@gmail.com>
2026-09-22 23:55:25 +0530

    Time: 25 ms (18.02%), Space: 81.3 MB (96.65%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -3,9 +3,9 @@ class Solution {
         
         Arrays.sort(nums);
         for(int i = 0; i < nums.length - 1; i++){
-                if(nums[i] == nums[i + 1]){
-                    return true;
-                }
+            if(nums[i] == nums[i+1]){
+                return true;
+            }
         }
         return false;
     }
